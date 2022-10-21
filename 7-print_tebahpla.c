73b8cb64bdd7f62daba6b6291325ec5e08714e28
Nj3r1 <susannganga884@gmail.com>
2022-10-21 14:07:31 -0700

    7-print_tebapla

---
+++ 7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include	<sstdio.h>
+
+/**
+ * main	-	Entry	point
+ *
+ * Return:	Always	0
+ */
+int	main	(void)
+{
+	char	letter:
+
+	for	(letter	=	'z';	letter	>=	'a';	letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return	(0);
+}
+
