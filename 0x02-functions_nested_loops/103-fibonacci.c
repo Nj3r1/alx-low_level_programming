c0bff9c03f5a5c94a42eea0d15c550cdcddd600b
Nj3r1 <susannganga884@gmail.com>
2022-10-26 19:39:38 +0300

    nested loops

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * purpose - fibonacci
+ *
+ * Return: (0) always
+ */
+int main(void)
+{
+	unsigned long int lim = 4000000;
+	unsigned long int bef = 0;
+	unsigned long int aft = 1;
+	unsigned long int R = 0;
+	unsigned long int sum = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
