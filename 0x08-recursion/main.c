8b1c3594fd9fcf09c64c0e4212656bb901f1afa3
Nj3r1 <susannganga884@gmail.com>
2023-06-09 22:55:55 +0300

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -1,27 +1,30 @@
 #include "main.h"
-  2 #include <stdio.h>
-  3
-  4 /**
-  5  * main - check the code
-  6  *
-  7  * Return: Always 0.
-  8  */
-  9 int main(void)
- 10 {
- 11     int r;
- 12
- 13     r = is_prime_number(1);
- 14     printf("%d\n", r);
- 15     r = is_prime_number(1024);
- 16     printf("%d\n", r);
- 17     r = is_prime_number(16);
- 18     printf("%d\n", r);
- 19     r = is_prime_number(17);
- 20     printf("%d\n", r);
- 21     r = is_prime_number(25);
- 22     printf("%d\n", r);
- 23     r = is_prime_number(-1);
- 24     printf("%d\n", r);
- 25     r = is_prime_number(113);
- 26     printf("%d\n", r);
- 27     r = is_prime_number(7919);
+#include <stdio.h>
+ 
+   /**
+    * main - check the code
+    *
+    * Return: Always 0.
+    */
+int main(void)
+  {
+      int r;
+ 
+      r = is_prime_number(1);
+      printf("%d\n", r);
+      r = is_prime_number(1024);
+      printf("%d\n", r);
+      r = is_prime_number(16);
+      printf("%d\n", r);
+      r = is_prime_number(17);
+      printf("%d\n", r);
+      r = is_prime_number(25);
+      printf("%d\n", r);
+      r = is_prime_number(-1);
+      printf("%d\n", r);
+      r = is_prime_number(113);
+      printf("%d\n", r);
+      r = is_prime_number(7919);
+      printf("%d\n", r);
+      return (0);
+  }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,14 +1,14 @@
 #include <stdio.h>
-  2 #ifndef MAIN_H
-  3 #define MAIN_H
-  4
-  5 int _putchar(char c);
-  6 void _puts_recursion(char *s);
-  7 void _print_rev_recursion(char *s);
-  8 int _strlen_recursion(char *s);
-  9 int factorial(int n);
- 10 int _pow_recursion(int x, int y);
- 11 int _sqrt_recursion(int n);
- 12 int is_prime_number(int n);
- 13
- 14 #endif
+#ifndef MAIN_H
+#define MAIN_H
+  
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+
+#endif
