813b597ba6566216db2c89594f40ae5d6e935f69
Nj3r1 <susannganga884@gmail.com>
2023-06-13 10:16:55 +0300

    Your subconscious is looking for the dreamer

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,10 +1,23 @@
 #include "main.h"
-/**
+ 
+  /*
   * Function that returns a factorial of a given number
   *
   * Return: int factorial
   */
- 
+
 int factorial(int n)
+{
+	 if (n == 0)
+{
+		 return (0);
 	}
+	 	else 
+		{
+			return (n * factorial(n - 1));
+
+		}
+}
+	
+
 
